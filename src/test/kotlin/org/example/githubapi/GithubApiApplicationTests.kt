aeef8cead2a7b974f98d1440e0546c9295957991
Piotr Wilkosz <pwilkosz99@gmail.com>
2024-02-22 18:09:12 +0100

    Project init

---
+++ src/test/kotlin/org/example/githubapi/GithubApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.githubapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
